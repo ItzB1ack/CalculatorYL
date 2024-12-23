0d2b3bdf649adef237a9cc6a216cbece93b868e3
ItzB1ack <125679043+ItzB1ack@users.noreply.github.com>
2024-12-23 15:12:48 +0300

    Update application_test.go

--- internal/application/application_test.go
+++ internal/application/application_test.go
@@ -7,7 +7,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/TravellerGSF/CalcGO/internal/application"
+	"github.com/ItzB1ack/CalculatorYL/internal/application"
 )
 
 func TestCalcHandler(t *testing.T) {
